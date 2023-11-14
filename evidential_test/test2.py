b551e1cf98491da1ac18baa1975d7106f8c613de
Christian Grannemann <grannemann@stud.uni-hannover.de>
2023-11-14 10:22:32 +0100

    mac commit

--- evidential_test/test2.py
+++ evidential_test/test2.py
@@ -1,5 +1,7 @@
 import torch
+import math
 import torch.nn as nn
+import torch.optim as optim
 
 class DERLayer(nn.Module):
     def __init__(self):
@@ -39,12 +41,37 @@ class TestModel(nn.Module):
 
         return x
 
-# Create an instance of your model
-model = TestModel()
+def loss_der(y, y_pred, coeff):
+    gamma, nu, alpha, beta = y[:, 0], y[:, 1], y[:, 2], y[:, 3]
+    error = gamma - y_pred
+    omega = 2.0 * beta * (1.0 + nu)
+
+    return torch.mean(
+        0.5 * torch.log(math.pi / nu)
+        - alpha * torch.log(omega)
+        + (alpha + 0.5) * torch.log(error**2 * nu + omega)
+        + torch.lgamma(alpha)
+        - torch.lgamma(alpha + 0.5)
+        + coeff * torch.abs(error) * (2.0 * nu + alpha)
+    )
+
 
 # Pass your tensor through the model
 input_tensor = torch.randn(1, 100, 128, 160)
-output_tensor = model(input_tensor)
+ground_truth = torch.randn(1)
+number_steps = 50
+
+# Create an instance of your model
+model = TestModel()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+optimizer.zero_grad()
+
+prediction = model(input_tensor)
+loss = loss_der(prediction, 1, coeff=0.01)
+
+loss.backward()
+optimizer.step()
 
 # Check the output shape
-print(output_tensor.shape)
+print(prediction.shape)
